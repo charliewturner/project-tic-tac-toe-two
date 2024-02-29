8139b23311b7fd808af3b9254d628ef7968eb5ae
Charlie Turner <c.w.turner@hotmail.com>
2024-02-29 21:03:39 +0000

    Continue writing functional skeleton

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-const gameboard = (function(){
-    
-    //gameboard shoudl contain an array of arrays 
+const gameboard = (function () {
+
+    //gameboard should contain an array of arrays 
     //containing the content of the boxes (3x3 empty to start)
     let gameboard = [["", "", ""],
-                     ["", "", ""],
-                     ["", "", ""]];
+    ["", "", ""],
+    ["", "", ""]];
 
-    function resetBoard(){
+    function resetBoard() {
         //clear contents of gameboard arrays
+        gameboard = [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]];
+        renderBoard();
     }
 
-    function displayControls(){
+    function renderBoard() {
+        //render board on HTML
+    }
+
+
+    function displayControls() {
         //display control elements on HTML
+        //move into renderBoard?
 
     }
 
-    function startGame(){
-
+    function startGame() {
+        resetBoard();
+        playGame();
     }
-
-
-
-
-
 
 });
 
-const playGame = (function (){
+const playGame = (function () {
 
     const players = {
         playerOne: "",
         playerTwo: ""
+
     };
 
+    const updatePlayers = () => {
+        players.playerOne = "";
+        players.playerTwo = "";
+
+        //set player names using user input
+    }
+
     //define functions to play the game
+    const checkWin = () => {
+        //check if a line of three is complete
+
+        //check which player won
+
+    }
+
+    const playerTurn = () => {
+
+    }
 
 
 
