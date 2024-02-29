9e11675a909308276427f379d0f5f8d57e59c4d0
Charlie Turner <c.w.turner@hotmail.com>
2024-02-29 20:52:34 +0000

    Write initial basis for factories

---
+++ script.js
@@ -0,0 +1,42 @@
+const gameboard = (function(){
+    
+    //gameboard shoudl contain an array of arrays 
+    //containing the content of the boxes (3x3 empty to start)
+    let gameboard = [["", "", ""],
+                     ["", "", ""],
+                     ["", "", ""]];
+
+    function resetBoard(){
+        //clear contents of gameboard arrays
+    }
+
+    function displayControls(){
+        //display control elements on HTML
+
+    }
+
+    function startGame(){
+
+    }
+
+
+
+
+
+
+});
+
+const playGame = (function (){
+
+    const players = {
+        playerOne: "",
+        playerTwo: ""
+    };
+
+    //define functions to play the game
+
+
+
+})
+
+gameboard();
